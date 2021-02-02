c58972b7d785d0a901b19e56862c2c83b8a4a234
hhwaaaaa1 <hhwaaaaa1@naver.com>
2021-02-02 23:08:06 +0900

    Browser-sync 설정

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,14 @@ const sourcemaps = require("gulp-sourcemaps");
 const scss = require("gulp-sass");
 const autoprefixer = require("autoprefixer");
 const postcss = require("gulp-postcss");
+const browserSync = require("browser-sync").create();
 
 const src = "./src";
 const dist = "./dist";
 const paths = {
-  pug: src + "/views/**/*.pug",
-  scss: src + "/styles/**/*.scss",
+  pug: `${src}/views/**/*.pug`,
+  scss: `${src}/styles/**/*.scss`,
 };
-
-gulp.task("clean", function () {
-  return gulp.src("./dist/*").pipe(clean({ force: true }));
-});
 
 /**
  * Pug:compile
@@ -27,7 +24,8 @@ gulp.task("pug:compile", function () {
   return gulp
     .src(paths.pug)
     .pipe(pug(pugOptions))
-    .pipe(gulp.dest(dist + "/html"));
+    .pipe(gulp.dest(`${dist}/html`))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 /**
@@ -46,7 +44,24 @@ gulp.task("scss:compile", function () {
     .pipe(scss(scssOptions).on("error", scss.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(dist + "/css"));
+    .pipe(gulp.dest(`${dist}/css`));
 });
 
-gulp.task("default", gulp.series(["clean", "pug:compile", "scss:compile"]));
+gulp.task("clean", function () {
+  return gulp.src(`${dist}/*`).pipe(clean({ force: true }));
+});
+
+gulp.task("server", function () {
+  browserSync.init({
+    server: `${dist}/html`,
+    directory: true,
+    port: 3001,
+    open: true,
+    tunnel: true,
+  });
+
+  gulp.watch(paths.pug, gulp.series(["clean", "pug:compile"]));
+  gulp.watch(paths.scss, gulp.series(["clean", "scss:compile"]));
+});
+
+gulp.task("default", gulp.series(["server"]));
